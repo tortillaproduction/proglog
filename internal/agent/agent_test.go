377da0f2ae2717808a9c66c86c82577c36b42899
tortillaproduction <momomo0206@gmail.com>
2026-07-29 18:31:04 +0900

    test(agent): update integration test for clustered Agent setupUpdate  to test multi-node Agent clustering with TLS, discovey, and Raft replication.

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/travisjeffery/go-dynaport"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 )
 
 func TestAgent(t *testing.T) {
@@ -63,6 +64,7 @@ func TestAgent(t *testing.T) {
 			ACLPolicyFile:   config.ACLPolicyFile,
 			ServerTLSConfig: serverTLSConfig,
 			PeerTLSConfig:   peerTLSConfig,
+			Bootstrap:       i == 0,
 		})
 		require.NoError(t, err)
 
@@ -114,18 +116,18 @@ func TestAgent(t *testing.T) {
 	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
 
 	// --- cyclic test of mutual data replication ---
-	// consumeResponse, err = leaderClient.Consume(
-	// 	context.Background(),
-	// 	&api.ConsumeRequest{
-	// 		Offset: produceResponse.Offset + 1,
-	// 	},
-	// )
-	// require.Nil(t, consumeResponse)
-	// require.Error(t, err)
+	consumeResponse, err = leaderClient.Consume(
+		context.Background(),
+		&api.ConsumeRequest{
+			Offset: produceResponse.Offset + 1,
+		},
+	)
+	require.Nil(t, consumeResponse)
+	require.Error(t, err)
 
-	// got := status.Code(err)
-	// want := status.Code(api.ErrorOffsetOutOfRange{}.GRPCStatus().Err())
-	// require.Equal(t, got, want)
+	got := status.Code(err)
+	want := status.Code(api.ErrorOffsetOutOfRange{}.GRPCStatus().Err())
+	require.Equal(t, got, want)
 }
 
 // --- helper ---
