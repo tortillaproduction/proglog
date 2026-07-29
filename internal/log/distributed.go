32e1c723291c4bd9c392d39d01ed60802e843f66
tortillaproduction <momomo0206@gmail.com>
2026-07-29 10:08:52 +0900

    feat(log): implement raft.LogStore wrapper using local log- adapter struct 'logStore' around 'Log' to implement the 'raft.LogStore' interface

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -184,6 +184,7 @@ func (l *DistributedLog) Read(offset uint64) (*api.Record, error) {
 }
 
 // --- fsm ---
+// ensure *fsm implements raft.FSM
 var _ raft.FSM = (*fsm)(nil)
 
 type fsm struct {
@@ -226,6 +227,7 @@ func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
 	return &snapshot{reader: r}, nil
 }
 
+// ensure *snapshot implements raft.FSMSnapshot
 var _ raft.FSMSnapshot = (*snapshot)(nil)
 
 type snapshot struct {
@@ -277,4 +279,65 @@ func (f *fsm) Restore(r io.ReadCloser) error {
 		buf.Reset()
 	}
 	return nil
+}
+
+// --- logStore ---
+// ensure *logStore implements raft.LogStore
+var _ raft.LogStore = (*logStore)(nil)
+
+type logStore struct {
+	*Log
+}
+
+func newLogStore(dir string, c Config) (*logStore, error) {
+	log, err := NewLog(dir, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &logStore{log}, nil
+}
+
+func (l *logStore) FirstIndex() (uint64, error) {
+	return l.LowestOffset()
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	off, err := l.HighestOffset()
+	return off, err
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		return err
+	}
+
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+
+	return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+	return l.StoreLogs([]*raft.Log{record})
+}
+
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		if _, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term:  record.Term,
+			Type:  uint32(record.Type),
+		}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	return l.Truncate(max)
 }
