6e5e180e77c9aeccd2ef04163fa12c5e27622c37
tortillaproduction <momomo0206@gmail.com>
2026-07-29 21:20:02 +0900

    feat(log): add GetServers RPC and method to retrieve Raft cluster statusAdd  to Protobuf service definition and implement it on  to query active cluster nodes and leader status.Key changes:- : Add // messages. Regenerate pb.go.- : implement  on  to fetch Raft configuration and identify the current leader.- : Update  to verify server counts and leader status before and after a node leaves ( method).

--- api/v1/log.pb.go
+++ api/v1/log.pb.go
@@ -265,6 +265,146 @@ func (x *ConsumeResponse) GetRecord() *Record {
 	return nil
 }
 
+type GetServersRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *GetServersRequest) Reset() {
+	*x = GetServersRequest{}
+	mi := &file_api_v1_log_proto_msgTypes[5]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *GetServersRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetServersRequest) ProtoMessage() {}
+
+func (x *GetServersRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_api_v1_log_proto_msgTypes[5]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetServersRequest.ProtoReflect.Descriptor instead.
+func (*GetServersRequest) Descriptor() ([]byte, []int) {
+	return file_api_v1_log_proto_rawDescGZIP(), []int{5}
+}
+
+type GetServersResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Servers       []*Server              `protobuf:"bytes,1,rep,name=servers,proto3" json:"servers,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *GetServersResponse) Reset() {
+	*x = GetServersResponse{}
+	mi := &file_api_v1_log_proto_msgTypes[6]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *GetServersResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*GetServersResponse) ProtoMessage() {}
+
+func (x *GetServersResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_api_v1_log_proto_msgTypes[6]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use GetServersResponse.ProtoReflect.Descriptor instead.
+func (*GetServersResponse) Descriptor() ([]byte, []int) {
+	return file_api_v1_log_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *GetServersResponse) GetServers() []*Server {
+	if x != nil {
+		return x.Servers
+	}
+	return nil
+}
+
+type Server struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Id            string                 `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	RpcAddr       string                 `protobuf:"bytes,2,opt,name=rpc_addr,json=rpcAddr,proto3" json:"rpc_addr,omitempty"`
+	IsLeader      bool                   `protobuf:"varint,3,opt,name=is_leader,json=isLeader,proto3" json:"is_leader,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *Server) Reset() {
+	*x = Server{}
+	mi := &file_api_v1_log_proto_msgTypes[7]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *Server) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Server) ProtoMessage() {}
+
+func (x *Server) ProtoReflect() protoreflect.Message {
+	mi := &file_api_v1_log_proto_msgTypes[7]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Server.ProtoReflect.Descriptor instead.
+func (*Server) Descriptor() ([]byte, []int) {
+	return file_api_v1_log_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *Server) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *Server) GetRpcAddr() string {
+	if x != nil {
+		return x.RpcAddr
+	}
+	return ""
+}
+
+func (x *Server) GetIsLeader() bool {
+	if x != nil {
+		return x.IsLeader
+	}
+	return false
+}
+
 var File_api_v1_log_proto protoreflect.FileDescriptor
 
 const file_api_v1_log_proto_rawDesc = "" +
@@ -282,12 +422,21 @@ const file_api_v1_log_proto_rawDesc = "" +
 	"\x0eConsumeRequest\x12\x16\n" +
 	"\x06offset\x18\x01 \x01(\x04R\x06offset\"9\n" +
 	"\x0fConsumeResponse\x12&\n" +
-	"\x06record\x18\x01 \x01(\v2\x0e.api.v1.RecordR\x06record2\x8f\x02\n" +
+	"\x06record\x18\x01 \x01(\v2\x0e.api.v1.RecordR\x06record\"\x13\n" +
+	"\x11GetServersRequest\">\n" +
+	"\x12GetServersResponse\x12(\n" +
+	"\aservers\x18\x01 \x03(\v2\x0e.api.v1.ServerR\aservers\"P\n" +
+	"\x06Server\x12\x0e\n" +
+	"\x02id\x18\x01 \x01(\tR\x02id\x12\x19\n" +
+	"\brpc_addr\x18\x02 \x01(\tR\arpcAddr\x12\x1b\n" +
+	"\tis_leader\x18\x03 \x01(\bR\bisLeader2\xd6\x02\n" +
 	"\x03Log\x12<\n" +
 	"\aProduce\x12\x16.api.v1.ProduceRequest\x1a\x17.api.v1.ProduceResponse\"\x00\x12<\n" +
 	"\aConsume\x12\x16.api.v1.ConsumeRequest\x1a\x17.api.v1.ConsumeResponse\"\x00\x12D\n" +
 	"\rConsumeStream\x12\x16.api.v1.ConsumeRequest\x1a\x17.api.v1.ConsumeResponse\"\x000\x01\x12F\n" +
-	"\rProduceStream\x12\x16.api.v1.ProduceRequest\x1a\x17.api.v1.ProduceResponse\"\x00(\x010\x01B5Z3github.com/tortillaproduction/proglog/api/v1;log_v1b\x06proto3"
+	"\rProduceStream\x12\x16.api.v1.ProduceRequest\x1a\x17.api.v1.ProduceResponse\"\x00(\x010\x01\x12E\n" +
+	"\n" +
+	"GetServers\x12\x19.api.v1.GetServersRequest\x1a\x1a.api.v1.GetServersResponse\"\x00B5Z3github.com/tortillaproduction/proglog/api/v1;log_v1b\x06proto3"
 
 var (
 	file_api_v1_log_proto_rawDescOnce sync.Once
@@ -301,30 +450,36 @@ func file_api_v1_log_proto_rawDescGZIP() []byte {
 	return file_api_v1_log_proto_rawDescData
 }
 
-var file_api_v1_log_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_api_v1_log_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_api_v1_log_proto_goTypes = []any{
-	(*Record)(nil),          // 0: api.v1.Record
-	(*ProduceRequest)(nil),  // 1: api.v1.ProduceRequest
-	(*ProduceResponse)(nil), // 2: api.v1.ProduceResponse
-	(*ConsumeRequest)(nil),  // 3: api.v1.ConsumeRequest
-	(*ConsumeResponse)(nil), // 4: api.v1.ConsumeResponse
+	(*Record)(nil),             // 0: api.v1.Record
+	(*ProduceRequest)(nil),     // 1: api.v1.ProduceRequest
+	(*ProduceResponse)(nil),    // 2: api.v1.ProduceResponse
+	(*ConsumeRequest)(nil),     // 3: api.v1.ConsumeRequest
+	(*ConsumeResponse)(nil),    // 4: api.v1.ConsumeResponse
+	(*GetServersRequest)(nil),  // 5: api.v1.GetServersRequest
+	(*GetServersResponse)(nil), // 6: api.v1.GetServersResponse
+	(*Server)(nil),             // 7: api.v1.Server
 }
 var file_api_v1_log_proto_depIdxs = []int32{
 	0, // 0: api.v1.ProduceRequest.record:type_name -> api.v1.Record
 	0, // 1: api.v1.ConsumeResponse.record:type_name -> api.v1.Record
-	1, // 2: api.v1.Log.Produce:input_type -> api.v1.ProduceRequest
-	3, // 3: api.v1.Log.Consume:input_type -> api.v1.ConsumeRequest
-	3, // 4: api.v1.Log.ConsumeStream:input_type -> api.v1.ConsumeRequest
-	1, // 5: api.v1.Log.ProduceStream:input_type -> api.v1.ProduceRequest
-	2, // 6: api.v1.Log.Produce:output_type -> api.v1.ProduceResponse
-	4, // 7: api.v1.Log.Consume:output_type -> api.v1.ConsumeResponse
-	4, // 8: api.v1.Log.ConsumeStream:output_type -> api.v1.ConsumeResponse
-	2, // 9: api.v1.Log.ProduceStream:output_type -> api.v1.ProduceResponse
-	6, // [6:10] is the sub-list for method output_type
-	2, // [2:6] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	7, // 2: api.v1.GetServersResponse.servers:type_name -> api.v1.Server
+	1, // 3: api.v1.Log.Produce:input_type -> api.v1.ProduceRequest
+	3, // 4: api.v1.Log.Consume:input_type -> api.v1.ConsumeRequest
+	3, // 5: api.v1.Log.ConsumeStream:input_type -> api.v1.ConsumeRequest
+	1, // 6: api.v1.Log.ProduceStream:input_type -> api.v1.ProduceRequest
+	5, // 7: api.v1.Log.GetServers:input_type -> api.v1.GetServersRequest
+	2, // 8: api.v1.Log.Produce:output_type -> api.v1.ProduceResponse
+	4, // 9: api.v1.Log.Consume:output_type -> api.v1.ConsumeResponse
+	4, // 10: api.v1.Log.ConsumeStream:output_type -> api.v1.ConsumeResponse
+	2, // 11: api.v1.Log.ProduceStream:output_type -> api.v1.ProduceResponse
+	6, // 12: api.v1.Log.GetServers:output_type -> api.v1.GetServersResponse
+	8, // [8:13] is the sub-list for method output_type
+	3, // [3:8] is the sub-list for method input_type
+	3, // [3:3] is the sub-list for extension type_name
+	3, // [3:3] is the sub-list for extension extendee
+	0, // [0:3] is the sub-list for field type_name
 }
 
 func init() { file_api_v1_log_proto_init() }
@@ -338,7 +493,7 @@ func file_api_v1_log_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_api_v1_log_proto_rawDesc), len(file_api_v1_log_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -249,6 +249,23 @@ func (l *DistributedLog) Close() error {
 	return l.log.Close()
 }
 
+func (l *DistributedLog) GetServers() ([]*api.Server, error) {
+	future := l.raft.GetConfiguration()
+	if err := future.Error(); err != nil {
+		return nil, err
+	}
+
+	var servers []*api.Server
+	for _, server := range future.Configuration().Servers {
+		servers = append(servers, &api.Server{
+			Id:       string(server.ID),
+			RpcAddr:  string(server.Address),
+			IsLeader: l.raft.Leader() == server.Address,
+		})
+	}
+	return servers, nil
+}
+
 // --- fsm ---
 // ensure *fsm implements raft.FSM
 var _ raft.FSM = (*fsm)(nil)
